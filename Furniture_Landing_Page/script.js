3e7d28c454c7eead360999d275161735c29300bd
Shaktisinh0044 <spparmar045@gmail.com>
2026-01-08 09:28:36 +0530

    new project

---
+++ Furniture_Landing_Page/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  effect: "fade",
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  fadeEffect: {
+    crossFade: true,
+  },
+});
